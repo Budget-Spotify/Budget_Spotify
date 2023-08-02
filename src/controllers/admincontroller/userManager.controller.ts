eb3c63a312b49035c4184fe725419a1953422441
KY <nguyenhoangky23121997@gmail.com>
2023-08-02 16:59:13 +0700

    editcontroller

--- src/controllers/admincontroller/userManager.controller.ts
+++ src/controllers/admincontroller/userManager.controller.ts
@@ -4,18 +4,24 @@ class UsersManagerController {
     static async getUserList(req: any, res: any) {
         try {
             let userList = await Users.find()
-            if (userList) {
-                res.status(200).json(userList)
+            if (userList.length === 0) {
+                const data = {
+                    status: 'empty',
+                    message: 'there is no user in the List'
+                }
+                return res.status(200).json(data)
             } else {
-                res.json({
-                    status:"failed",
-                    message: "there is something wrong"
+                res.status(200).json({
+                    status:'full',
+                    list: userList
                 })
             }
-        }catch(err){
-            console.log(err);
+        } catch (err) {
+            res.status(500).json({
+                message: "Failed"
+            })
         }
-   }
+    }
 }
 
 

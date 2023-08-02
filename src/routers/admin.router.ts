eca9f60319ea77465c6f429b3db42ea4f65c9457
Lã Ngọc Hải <langochai11121998@gmail.com>
2023-08-02 15:12:33 +0700

    Update admin.router.ts

--- src/routers/admin.router.ts
+++ src/routers/admin.router.ts
@@ -2,8 +2,6 @@ import express from "express";
 import UsersManagerController from "../controllers/admincontroller/userManager.controller";
 const adminApiRouter = express.Router()
 
-adminApiRouter.get('/users',(req,res)=>{
-    UsersManagerController.getUserList(req,res)
-})
+adminApiRouter.get('/users',UsersManagerController.getUserList)
 
 export default adminApiRouter

a0196cc5cadd6a71e0a3a612170c713288364cc0
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-08-03 10:46:39 +0700

    get songs uploaded

--- src/routers/userRouter/userRouter.ts
+++ src/routers/userRouter/userRouter.ts
@@ -3,4 +3,5 @@ import userController from "../../controllers/userController/user.controller";
 const userRouter = express.Router();
 
 userRouter.post('/upload/song',userController.addSong)
+userRouter.get('/list/songs',userController.getSongs);
 export default userRouter

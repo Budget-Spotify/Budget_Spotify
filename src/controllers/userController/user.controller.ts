0c97893b171ab976108f8b17c422d1387529dffb
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-08-03 17:08:48 +0700

    get one song uploaded

--- src/controllers/userController/user.controller.ts
+++ src/controllers/userController/user.controller.ts
@@ -46,6 +46,26 @@ class UserController {
             res.status(404).json({status: "failed", message: err.message});
         }
     }
+
+    static async getOneSong(req, res) {
+        try {
+            let songId = req.params.id;
+            let song = await Songs.findOne({_id: songId});
+            if (song) {
+                res.status(200).json({
+                    status: 'succeeded',
+                    song: song
+                })
+            } else {
+                res.status(404).json({
+                    status: 'failed',
+                    message: 'No data'
+                });
+            }
+        } catch (err) {
+            res.status(404).json({status: "failed", message: err.message});
+        }
+    }
 }
 
 export default UserController

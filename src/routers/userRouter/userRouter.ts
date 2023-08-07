ed09fd133beb7243a269656e960804549d654685
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-08-07 14:15:22 +0700

    delete song

--- src/routers/userRouter/userRouter.ts
+++ src/routers/userRouter/userRouter.ts
@@ -7,9 +7,10 @@ const userRouter = express.Router();
 userRouter.post('/upload/song', userController.addSong)
 userRouter.get('/list/songs', userController.getSongs);
 userRouter.get('/song/detail/:id', userController.getOneSong);
-userRouter.post('/upload/song',userController.addSong)
-userRouter.get('/list/songs',userController.getSongs);
-userRouter.get('/info',userController.getDetail)
-userRouter.put('/editpassword',userController.editPassword)
-userRouter.put('/editinfo',userController.editInfo)
+userRouter.post('/upload/song', userController.addSong)
+userRouter.get('/list/songs', userController.getSongs);
+userRouter.get('/info', userController.getDetail)
+userRouter.put('/editpassword', userController.editPassword)
+userRouter.put('/editinfo', userController.editInfo)
+userRouter.delete('/song/delete', userController.deleteSong);
 export default userRouter

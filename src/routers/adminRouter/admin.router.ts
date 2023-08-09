8feaf3a2336c589de678d6819bbee28935b13ea4
KY <nguyenhoangky23121997@gmail.com>
2023-08-09 09:43:44 +0700

    pull dev

--- src/routers/adminRouter/admin.router.ts
+++ src/routers/adminRouter/admin.router.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import UsersManagerController from "../../controllers/admincontroller/userManager.controller";
+import { Security } from "../../security/security";
 const adminApiRouter = express.Router()
-
+adminApiRouter.use(Security.checkAdmin)
 adminApiRouter.get('/userlist', UsersManagerController.getUserList)
-
 export default adminApiRouter
--- src/routers/userRouter/userRouter.ts
+++ src/routers/userRouter/userRouter.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import userController from "../../controllers/userController/user.controller";
-import {Security} from "../../security/security";
+
 
 const userRouter = express.Router();
-
 userRouter.post('/upload/song', userController.addSong)
 userRouter.get('/list/songs', userController.getSongs);
 userRouter.get('/song/detail/:id', userController.getOneSong);

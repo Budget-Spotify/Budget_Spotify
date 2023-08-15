c9b1e302ae3ac7f9107a87417f44c57dac5cb0ec
langochai <langochai11121998@gmail.com>
2023-08-15 15:03:38 +0700

    fix add playlist not resetting form and fix get random song

--- src/routers/songRouter/songRouter.ts
+++ src/routers/songRouter/songRouter.ts
@@ -5,7 +5,7 @@ const songRouter = express.Router();
 
 songRouter.get('/list/songs', SongController.getPublicSongs);
 songRouter.get('/search-public',SongController.searchSongPublic)
-songRouter.get('/random', SongController.getRandomSong);
+songRouter.post('/random', SongController.getRandomSong);
 songRouter.get('/singers', SongController.getSingers);
 songRouter.get('/composers', SongController.getComposers);
 songRouter.get('/tags', SongController.getTags);
